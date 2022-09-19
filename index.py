0c428b64828e26ca66a488ccd4d29a73287f7d51
Shubhrose Singh <shubhroses@gmail.com>
2022-09-19 15:32:01 -0700

    Fixed indents

--- index.py
+++ index.py
@@ -5,23 +5,27 @@ import collections
 import time
 
 class index:
-	def __init__(self,path):
+    def __init__(self,path):
         pass
 
-	def buildIndex(self):
+    def buildIndex(self):
         #function to read documents from collection, tokenize and build the index with tokens
         #index should also contain positional information of the terms in the document --- term: [(ID1,[pos1,pos2,..]), (ID2, [pos1,pos2,…]),….]
         #use unique document IDs
+
+        """
+        1. F
+        """
         pass
 
-	def and_query(self, query_terms):
+    def and_query(self, query_terms):
 	    #function for identifying relevant docs using the index
         pass
 
-	def print_dict(self):
+    def print_dict(self):
         #function to print the terms and posting list in the index
         pass
 
-	def print_doc_list(self):
+    def print_doc_list(self):
 	    # function to print the documents and their document id
         pass
